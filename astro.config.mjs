caa6bc12b7065019fd26923b38f3293d2f3339da
Pritesh Singh <pritesh.singh@utexas.edu>
2024-05-04 20:16:59 -0500

    removed dist from gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://quant-dot-ai.github.io/arkade/",
+  base: "/arkade/",
   integrations: [tailwind(), mdx(), sitemap()],
 });
