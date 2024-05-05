050ec22e402a99bf3c2966269264ac23f9802601
Pritesh Singh <146693417+quant-dot-ai@users.noreply.github.com>
2024-05-04 23:07:12 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  base: "/arkade/",
   integrations: [tailwind(), mdx(), sitemap()],
 });
